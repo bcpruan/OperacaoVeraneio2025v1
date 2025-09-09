327fb4bad6756dba177daf02fdd201e35aaf7d68
bcpruan <chefedesocorrolm@gmail.com>
2025-09-09 09:48:43 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+      caches.open('ov2025-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',
+          '/icon.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    e.respondWith(
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
+    );
+  });
+  
